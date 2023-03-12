15924f12d0f83a6fd7f60742ba835a31e5a1852c
Thanos Alevizos <thanos.alev@gmail.com>
2023-03-12 17:49:13 +0000

    Send emails using the form

--- Udemy/app2-portfolio/send_email.py
+++ Udemy/app2-portfolio/send_email.py
@@ -1,20 +1,17 @@
-import smtplib
-import ssl
+import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "thanos.auto90@gmail.com"
-password = "gboagczwziqisxvj"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "thanos.auto90@gmail.com"
+    password = "gboagczwziqisxvj"
+    receiver = "thanos.alev@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "thanos.alev@gmail.com"
-context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-message = """\
-Subject:Hi!
-How are you? 
-Bye!
-"""
-with smtplib.SMTP_SSL(host,port,context = context) as server:
-    server.login(username,password)
-    server.sendmail(username, receiver, message)
+
+
